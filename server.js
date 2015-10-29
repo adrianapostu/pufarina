d59494c635b535f1e1998e2c988a47cd2571b8a9
Adrian <adrian.apostu31@gmail.com>
2015-10-30 01:40:47 +0200

    working

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var bodyParser = require('body-parser');
 var five = require("johnny-five");
 var cors = require('cors');
 var Edison = require("edison-io");
+var path = require('path');
 
 // initialise Edison baord and LED pins
 
@@ -11,6 +12,11 @@ var app = express();
 //middleware
 app.use(cors())
 app.use(bodyParser.json());
+
+app.get('/', function(req, res)
+  {
+    res.sendFile(path.join(__dirname, '/index.html'));
+  });
 
 // post requests
 // {id} is a param, usually a number
